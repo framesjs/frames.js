5fced01dee7527dbe6c69748853656aba3474b2d
David Furlong <dvfurlong@googlemail.com>
2024-02-02 17:16:44 +0100

    fix: github link

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -4,4 +4,20 @@ export default defineConfig({
   ogImageUrl:
     "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
   title: "frames.js",
+  topNav: [
+    { text: "Github", link: "https://github.com/framesjs/frames.js" },
+    // {
+    //   text: version,
+    //   items: [
+    //     {
+    //       text: "Changelog",
+    //       link: "https://github.com/wevm/vocs/blob/main/src/CHANGELOG.md",
+    //     },
+    //     {
+    //       text: "Contributing",
+    //       link: "https://github.com/wevm/vocs/blob/main/.github/CONTRIBUTING.md",
+    //     },
+    //   ],
+    // },
+  ],
 });
