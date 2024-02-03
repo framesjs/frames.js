87bdc8c830ab7fbc8f77d911db741e6fe42bc153
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-02-04 01:09:42 +0200

    fix: indep build

--- examples/framesjs-starter/.eslintrc.js
+++ examples/framesjs-starter/.eslintrc.js
@@ -1,9 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@framesjs/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+  extends: ["nextjs"],
 };
