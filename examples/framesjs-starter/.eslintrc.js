a1665ea6b1e462c978703f872b0f890a18d7c996
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-02-03 22:47:49 +0200

    fix: build

--- examples/framesjs-starter/.eslintrc.js
+++ examples/framesjs-starter/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@framejs/eslint-config/next.js"],
+  extends: ["@framesjs/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
