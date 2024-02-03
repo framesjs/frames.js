10a79e93c3114458519de6f1a82b54ad87aa1a68
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-02-03 14:54:45 +0200

    fix: validation via hub REST API

--- packages/frames.js/src/validateFrameMessage.ts
+++ packages/frames.js/src/validateFrameMessage.ts
@@ -6,9 +6,9 @@ export async function validateFrameMessage(body: FrameActionPayload): Promise<{
   message: FrameActionMessage | undefined;
 }> {
   const hubBaseUrl =
-    process.env.FRAME_HUB_URL ||
-    process.env.HUB_URL ||
-    "nemes.farcaster.xyz:2283";
+    process.env.FRAME_HUB_HTTP_URL ||
+    process.env.HUB_HTTP_URL ||
+    "https://nemes.farcaster.xyz:2281";
 
   const validateMessageResponse = await fetch(
     `${hubBaseUrl}/v1/validateMessage`,
