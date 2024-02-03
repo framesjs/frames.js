56b832a8f4c4bab688d1e721ae971434ad9bee4e
David Furlong <dvfurlong@googlemail.com>
2024-02-03 23:41:30 +0100

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["examples/**", "packages/**"],
   extends: ["@framesjs/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
