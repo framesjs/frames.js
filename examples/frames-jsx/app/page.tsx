111eb64b367c842dfc3c735b4f5fd133f6a502eb
David Furlong <dvfurlong@googlemail.com>
2024-02-03 12:10:25 +0100

    chore: commit progress

--- examples/frames-jsx/app/page.tsx
+++ examples/frames-jsx/app/page.tsx
@@ -1,32 +1,59 @@
-import { FFrame, FFrameImage, FFrameButton } from "frames.js";
+import {
+  FFrame,
+  FFrameImage,
+  FFrameButton,
+  FrameReducer,
+  useFramesReducer,
+  createFrameContextNextjs,
+  validateFrameMessageOrThrow,
+} from "frames.js";
 
+type State = {
+  active: string;
+};
+
+const initialState = { active: "1" };
+
+const reducer: FrameReducer<State> = (state, action) => {
+  return {
+    active: action.frame_action_received?.untrustedData.buttonIndex
+      ? String(action.frame_action_received?.untrustedData.buttonIndex)
+      : "1",
+  };
+};
+
+// This is a react server component only
 export default async function Home({
-  searchParams: frameState,
+  searchParams,
 }: {
   searchParams: Record<string, string>;
 }) {
-  // Do a server side side effect either sync or async, such as minting an NFT
-  if (frameState.x === "boris") {
-    // example: load the users credentials & check they have an NFT
-    await Promise.resolve();
-  }
+  const frameContext = createFrameContextNextjs<State>(searchParams);
+  await validateFrameMessageOrThrow(frameContext.frame_action_received);
+  const [state] = useFramesReducer<State>(reducer, initialState, frameContext);
 
-  // return next frame
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+
+  // then, when done, return next frame
   return (
-    <FFrame postUrl="">
-      <FFrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
-      <FFrameButton onClick={() => ({ ...frameState, x: "1" })}>
-        {frameState?.x === "1" ? "Active" : "Inactive"}
-      </FFrameButton>
-      <FFrameButton onClick={() => ({ x: "2" })}>
-        {frameState?.x === "2" ? "Active" : "Inactive"}
-      </FFrameButton>
-      <FFrameButton onClick={() => ({ y: "hello" })}>
-        {frameState?.y === "hello" ? "Active" : "Inactive"}
-      </FFrameButton>
-      <FFrameButton onClick={() => ({ x: "boris" })}>
-        {frameState?.x === "boris" ? "Active" : "Inactive"}
-      </FFrameButton>
-    </FFrame>
+    <div>
+      Frames-jsx example
+      <FFrame postUrl="">
+        <FFrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
+        <FFrameButton>
+          {state?.active === "1" ? "Active" : "Inactive"}
+        </FFrameButton>
+        <FFrameButton>
+          {state?.active === "2" ? "Active" : "Inactive"}
+        </FFrameButton>
+        <FFrameButton>
+          {state?.active === "3" ? "Active" : "Inactive"}
+        </FFrameButton>
+        <FFrameButton>
+          {state?.active === "4" ? "Active" : "Inactive"}
+        </FFrameButton>
+      </FFrame>
+    </div>
   );
 }
--- packages/frames.js/src/nextjs.tsx
+++ packages/frames.js/src/nextjs.tsx
@@ -1,20 +1,144 @@
-import React, { useCallback } from "react";
-import { ActionIndex } from "./types";
+import React, { useReducer } from "react";
+import { ActionIndex, FrameActionPayload } from "./types";
+import { NextRequest, NextResponse } from "next/server";
+import { validateFrameMessage } from ".";
+import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+import { headers } from "next/headers";
 
-type FrameState = Record<string, string>;
+export type FrameState = Record<string, string>;
+
+export type FrameElementType =
+  | typeof FFrameButton
+  | typeof FFrameImage
+  | typeof FFFrameInput;
+
+export type FrameContext<T extends FrameState = FrameState> = {
+  frame_action_received: FrameActionPayload | null;
+  frame_prev_state: T | null;
+  pathname?: string;
+  url: string;
+  headers: ReadonlyHeaders;
+};
+
+type StringifiedValues<T> = {
+  [K in keyof T]?: string;
+};
+
+export type FFrameUrlSearchParamsFlattened = StringifiedValues<FrameContext>;
+
+export type FrameReducer<T extends FrameState = FrameState> = (
+  state: T,
+  action: FrameContext
+) => T;
+
+export async function validateFrameMessageOrThrow(
+  frameActionPayload: FrameActionPayload | null
+) {
+  if (!frameActionPayload) {
+    // no payload means no action
+    return;
+  }
+  const { isValid } = await validateFrameMessage(frameActionPayload);
+  if (!isValid) {
+    throw new Error("frames.js: signature failed verification");
+  }
+}
+
+export function createFrameContextNextjs<T extends FrameState = FrameState>(
+  searchParams: Record<string, string>
+): FrameContext<T> {
+  return createFrameContext(parseFrameParams<T>(searchParams), headers());
+}
+
+export function createFrameContext<T extends FrameState = FrameState>(
+  frameContextFromParams: Pick<
+    FrameContext<T>,
+    "frame_action_received" | "frame_prev_state" | "pathname"
+  >,
+  headers: ReadonlyHeaders
+): FrameContext<T> {
+  return {
+    ...frameContextFromParams,
+    headers: headers,
+    // fixme:
+    url: "",
+  };
+}
+
+export function parseFrameParams<T extends FrameState = FrameState>(
+  searchParams: Record<string, string>
+): Pick<
+  FrameContext<T>,
+  "frame_action_received" | "frame_prev_state" | "pathname"
+> {
+  const frameActionReceived = searchParams.frame_action_received
+    ? (JSON.parse(searchParams.frame_action_received) as FrameActionPayload)
+    : null;
+
+  const framePrevState = searchParams.frame_prev_state
+    ? (JSON.parse(searchParams.frame_prev_state) as T)
+    : null;
+
+  return {
+    frame_action_received: frameActionReceived,
+    frame_prev_state: framePrevState,
+    pathname: searchParams.pathname,
+  };
+}
+
+export function useFramesReducer<T extends FrameState = FrameState>(
+  reducer: FrameReducer<T>,
+  initialState: T,
+  initializerArg: FrameContext<T>
+): [T, React.Dispatch<FrameContext<T>>] {
+  function frameReducerInit(initial: FrameContext<T>): T {
+    if (
+      initial.frame_prev_state === null ||
+      initial.frame_action_received === null
+    )
+      return initialState;
+
+    return reducer(initial.frame_prev_state, initial);
+  }
+
+  // FIXME
+  function dispatch() {}
+
+  return [frameReducerInit(initializerArg), dispatch];
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const { searchParams } = new URL(req.url);
+  const generatedUrlWithNextFrameState = new URL(
+    searchParams.get("pathname") || "/"
+  );
+
+  generatedUrlWithNextFrameState.searchParams.set(
+    "frame_action_received",
+    JSON.stringify(body)
+  );
+  generatedUrlWithNextFrameState.searchParams.set(
+    "frame_prev_state",
+    searchParams.get("frame_prev_state") ?? ""
+  );
+
+  return NextResponse.redirect(generatedUrlWithNextFrameState.toString());
+}
 
 export function FFrame({
   children,
   postUrl,
 }: {
   postUrl: string;
-  children: Array<
-    | React.ReactElement<typeof FFrameButton>
-    | React.ReactElement<typeof FFrameImage>
-  >;
+  children: Array<React.ReactElement<FrameElementType>>;
 }) {
-  let nextButtonIndex: ActionIndex = 1;
-  let hasImage = false;
+  const nextIndexByComponentType = {
+    button: 1,
+    image: 1,
+    input: 1,
+  };
   const newTree = (
     <>
       <meta name="fc:frame" content="vNext" />
@@ -26,23 +150,36 @@ export function FFrame({
               return child;
             }
 
+            if (nextIndexByComponentType.button > 4) {
+              throw new Error("too many buttons");
+            }
+
             if (child.props.hasOwnProperty("href")) {
               return (
                 <FFrameRedirect
                   {...(child.props as any)}
-                  actionIndex={nextButtonIndex++}
+                  actionIndex={nextIndexByComponentType.button++}
                 />
               );
             } else {
               return (
                 <FFrameButtonShim
                   {...(child.props as any)}
-                  actionIndex={nextButtonIndex++}
+                  actionIndex={nextIndexByComponentType.button++}
                 />
               );
             }
+          case FFFrameInput:
+            if (nextIndexByComponentType.input > 1) {
+              throw new Error("max one input allowed");
+            }
+            nextIndexByComponentType.input++;
+            return child;
           case FFrameImage:
-            hasImage = true;
+            if (nextIndexByComponentType.image > 1) {
+              throw new Error("max one image allowed");
+            }
+            nextIndexByComponentType.image++;
             return child;
           default:
             throw new Error(
@@ -53,7 +190,7 @@ export function FFrame({
     </>
   );
 
-  if (!hasImage)
+  if (nextIndexByComponentType.image === 1)
     throw new Error("an <FrameImage> element inside a <Frame> is required");
 
   return newTree;
@@ -68,17 +205,12 @@ type FrameButtonProvidedProps =
   | FrameButtonPostProvidedProps;
 
 type FrameButtonPostProvidedProps = {
-  onClick: () => FrameState;
   children: string | number;
 };
 type FrameButtonPostRedirectProvidedProps = {
   href: string;
   children: string | number;
 };
-
-function getUrlFromOnClick(frameState: FrameState) {
-  return `?state=${encodeURIComponent(JSON.stringify(frameState))}`;
-}
 
 export function FFrameRedirect({
   actionIndex,
@@ -105,9 +237,7 @@ export function FFrameButton(props: FrameButtonProvidedProps) {
 export function FFrameButtonShim({
   actionIndex,
   children,
-  onClick,
 }: FrameButtonPostProvidedProps & FrameButtonAutomatedProps) {
-  const url = getUrlFromOnClick(onClick);
   return (
     <>
       <meta
@@ -115,6 +245,14 @@ export function FFrameButtonShim({
         content={String(children)}
       />
       <meta name={`fc:frame:button:${actionIndex}:action`} content={"post"} />
+    </>
+  );
+}
+
+export function FFFrameInput({ text }: { text: string }) {
+  return (
+    <>
+      <meta name="fc:frame:input:text" content={text} />
     </>
   );
 }
