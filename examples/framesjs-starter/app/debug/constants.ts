7fabc88e13466e6130facbfd2adf5737dd93ec34
David Furlong <dvfurlong@googlemail.com>
2024-02-04 13:03:12 +0100

    fix: fallback host

--- examples/framesjs-starter/app/debug/constants.ts
+++ examples/framesjs-starter/app/debug/constants.ts
@@ -1,4 +1,4 @@
-export const HOST = process.env["NEXT_PUBLIC_HOST"];
+export const HOST = process.env["NEXT_PUBLIC_HOST"] || "http://localhost:3000";
 export const LOCAL_STORAGE_KEYS = {
   FARCASTER_USER: "farcasterUser",
 };
