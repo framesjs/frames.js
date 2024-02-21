edb01363cd7c65318aa5b75335153b46818f657b
David Furlong <dvfurlong@googlemail.com>
2024-02-21 12:19:14 +0100

    fix: docs homeframe

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -118,18 +118,6 @@ export default defineConfig({
     <>
       {/** on production this is rewritten by vercel */}
       <script defer src="/_vercel/insights/script.js" />
-      <meta property="og:type" content="website" />
-      <meta name="fc:frame" content="vNext" />
-      <meta
-        name="fc:frame:post_url"
-        content="https://framesjs-homeframe.vercel.app/frames?p=&amp;s=%7B%22page%22%3A1%7D&amp;r=%7B%7D"
-      />
-      <meta name="fc:frame:image" content="https://framesjs.org/og.png" />
-      <meta property="og:image" content="https://framesjs.org/og.png" />
-      <meta name="fc:frame:button:1:target" content="https://framesjs.org" />
-      <meta name="fc:frame:button:1:action" content="link" />
-      <meta name="fc:frame:button:2" content="→" />
-      <meta name="fc:frame:button:2:action" content="post" />
     </>
   ),
   sidebar: sidebar,
