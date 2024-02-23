09d1311f78d6e023188e5b326592db4127133512
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-02-23 11:04:09 +0100

    chore: remove unnecessary code

--- packages/debugger/bin/debugger.js
+++ packages/debugger/bin/debugger.js
@@ -80,15 +80,8 @@ createServer(async (req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    if (pathname === "/a") {
-      await app.render(req, res, "/a", query);
-    } else if (pathname === "/b") {
-      await app.render(req, res, "/b", query);
-    } else {
-      await handle(req, res, parsedUrl);
-    }
+    
+    await handle(req, res, parsedUrl);
   } catch (err) {
     console.error("Error occurred handling", req.url, err);
     res.statusCode = 500;
