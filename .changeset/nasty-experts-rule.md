8fdede071f3240145c4ea76b18faf27a13d1592c
Jimmy Madrigal <jimmymadrigal+github@gmail.com>
2024-03-10 09:09:28 -0600

    chore: publish frames.js artifact

---
+++ .changeset/nasty-experts-rule.md
@@ -0,0 +1,11 @@
+---
+"frames.js": minor
+"docs": minor
+"framesjs-starter": patch
+"utils-starter": patch
+"@frames.js/debugger": patch
+"@framesjs/eslint-config": patch
+"@framesjs/typescript-config": patch
+---
+
+Add DSCVR adapter
--- packages/frames.js/src/dscvr/index.ts
+++ packages/frames.js/src/dscvr/index.ts
@@ -1,16 +1,11 @@
 import {
   type DscvrFramesRequest,
   type DscvrClientProtocol,
-  type DscvrFrameActionData,
+  type DscvrValidationResponse,
   validateClientProtocol,
   validateFramesPost,
 } from "@dscvr-one/frames-adapter";
 import { FrameActionPayload } from "..";
-
-export type DscvrFrameMessageReturnType = DscvrFrameActionData & {
-  verifiedDscvrId: string;
-  verifiedContentId?: bigint;
-};
 
 export const isDscvrFrameActionPayload = (
   frameActionPayload: FrameActionPayload
@@ -23,16 +18,11 @@ export const isDscvrFrameActionPayload = (
 
 export const getDscvrFrameMessage = async (
   frameActionPayload: DscvrFramesRequest
-): Promise<DscvrFrameMessageReturnType> => {
-  const { actionBody, verifiedDscvrId, verifiedContentId } =
-    await validateFramesPost({
-      ...frameActionPayload,
-      clientProtocol: frameActionPayload.clientProtocol as DscvrClientProtocol,
-    });
+): Promise<DscvrValidationResponse> => {
+  const result = await validateFramesPost({
+    ...frameActionPayload,
+    clientProtocol: frameActionPayload.clientProtocol as DscvrClientProtocol,
+  });
 
-  return {
-    ...actionBody,
-    verifiedDscvrId,
-    verifiedContentId,
-  };
+  return result;
 };
