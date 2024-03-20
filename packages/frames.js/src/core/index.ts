906fc4889664dc07bc6b4d81aff3ddf7c9961c01
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-03-20 09:05:28 +0100

    build: export more stuff

--- packages/frames.js/src/core/index.ts
+++ packages/frames.js/src/core/index.ts
@@ -1,1 +1,6 @@
 export { createFrames } from "./createFrames";
+export { parallelizeMiddleware } from "./middleware/parallelizeMiddleware";
+export { composeMiddleware } from "./composeMiddleware";
+export { Button } from "./components";
+export { redirect } from "./redirect";
+export * from "./types";
