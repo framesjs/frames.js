700268f0a5757be107108640e3738abdbfc08df5
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-03-21 11:34:43 +0200

    fix: useCallback dependencies

--- packages/debugger/app/page.tsx
+++ packages/debugger/app/page.tsx
@@ -90,7 +90,7 @@ export default function App({
         return null;
       }
     },
-    []
+    [account.address, currentChainId, config, openConnectModal]
   );
 
   const frameState = useFrame({
