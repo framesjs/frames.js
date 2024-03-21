5228de7e287dc0bc99f1b7d29863c15c55247bf2
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-03-21 09:05:45 +0100

    fix: properly get request from handler arguments

--- packages/remix/src/index.ts
+++ packages/remix/src/index.ts
@@ -1,3 +1,22 @@
-export * from "frames.js/core";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { createFrames as coreCreateFrames } from "frames.js/core";
+export { Button, type types } from "frames.js/core";
 
 export { fetchMetaData } from "./fetchMetaData";
+
+// @todo provide better type that can change returned handler type
+export const createFrames: typeof coreCreateFrames =
+  function createFramesForNextJS(options: any) {
+    const frames = coreCreateFrames(options);
+
+    // @ts-expect-error
+    return function createHandler(handler, handlerOptions) {
+      const requestHandler = frames(handler, handlerOptions);
+
+      return function handleNextJSApiRequest({
+        request,
+      }: LoaderFunctionArgs | ActionFunctionArgs) {
+        return requestHandler(request);
+      };
+    };
+  } as unknown as typeof coreCreateFrames;
