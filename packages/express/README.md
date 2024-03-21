b4bb5c580e3e8508ee9f9207a5b7a5249b300fa2
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-03-21 13:25:33 +0100

    feat: express adapter

---
+++ packages/express/README.md
@@ -0,0 +1,39 @@
+# @frames.js/express
+
+Build your frames.js app using [Express.js](https://expressjs.com).
+
+## Installation
+
+```sh
+$ npm install @frames.js/express
+$ yarn add @frames.js/express
+$ pnpm add @frames.js/express
+```
+
+## Usage
+
+### Build your first Frames
+
+```tsx
+// app/routes/frames.tsx
+import { createFrames, Button } from "@frames.js/express";
+import express from "express";
+
+const app = express();
+
+const frames = createFrames();
+const framesRouteHandler = frames(async (ctx) => {
+  const didClickButton = !!ctx.clickedButton;
+
+  return {
+    image: (
+      <span>
+        {didClickButton ? "You clicked the button" : "Click the button"}
+      </span>
+    ),
+    buttons: [<Button action="post">Click me</Button>],
+  };
+});
+
+app.use("/frames", framesRouteHandler);
+```
---
+++ packages/express/src/index.test.tsx
@@ -0,0 +1,104 @@
+import express from "express";
+import request from "supertest";
+import * as lib from ".";
+import { FRAMES_META_TAGS_HEADER } from "frames.js/core";
+
+describe("express adapter", () => {
+  it.each(["createFrames", "Button"])("exports %s", (exportName) => {
+    expect(lib).toHaveProperty(exportName);
+  });
+
+  it("properly correctly integrates with express.js app", async () => {
+    const app = express();
+    const frames = lib.createFrames();
+    const expressHandler = frames(async ({ request }) => {
+      expect(request).toBeInstanceOf(Request);
+      expect(request.url).toMatch(/http:\/\/127\.0\.0\.1:\d+\//);
+
+      return {
+        image: <span>Nehehe</span>,
+        buttons: [
+          <lib.Button action="post" key="1">
+            Click me
+          </lib.Button>,
+        ],
+      };
+    });
+
+    app.use("/", expressHandler);
+
+    await request(app).get("/").expect("Content-Type", "text/html").expect(200);
+  });
+
+  it("properly correctly integrates with express.js app and returns JSON if asked to", async () => {
+    const app = express();
+    const frames = lib.createFrames();
+    const expressHandler = frames(async ({ request }) => {
+      expect(request).toBeInstanceOf(Request);
+      expect(request.url).toMatch(/http:\/\/127\.0\.0\.1:\d+\//);
+
+      return {
+        image: <span>Nehehe</span>,
+        buttons: [
+          <lib.Button action="post" key="1">
+            Click me
+          </lib.Button>,
+        ],
+      };
+    });
+
+    app.use("/", expressHandler);
+
+    await request(app)
+      .get("/")
+      .set("Accept", FRAMES_META_TAGS_HEADER)
+      .expect("Content-Type", "application/json")
+      .expect(200);
+  });
+
+  it("properly handles error response", async () => {
+    const app = express();
+    const frames = lib.createFrames();
+    const expressHandler = frames(async () => {
+      throw new Error("Something went wrong");
+    });
+
+    app.use("/", expressHandler);
+
+    await request(app)
+      .get("/")
+      .expect(500)
+      .expect("Content-type", "text/plain");
+  });
+
+  it("resolves button targets correctly", async () => {
+    const app = express();
+    const frames = lib.createFrames({ basePath: "/api" });
+    const expressHandler = frames(async ({ request }) => {
+      expect(request).toBeInstanceOf(Request);
+      expect(request.url).toMatch(/http:\/\/127\.0\.0\.1:\d+\//);
+
+      return {
+        image: <span>Nehehe</span>,
+        buttons: [
+          <lib.Button action="post" key="1" target="/nested">
+            Click me
+          </lib.Button>,
+        ],
+      };
+    });
+
+    app.use("/api", expressHandler);
+
+    await request(app)
+      .get("/api")
+      .set("Accept", FRAMES_META_TAGS_HEADER)
+      .expect(200)
+      .expect("Content-type", "application/json")
+      .expect((res) => {
+        expect(res.body["fc:frame:button:1:target"]).toMatch(
+          /http:\/\/127\.0\.0\.1:\d+\/api\/nested/
+        );
+      });
+  });
+});
