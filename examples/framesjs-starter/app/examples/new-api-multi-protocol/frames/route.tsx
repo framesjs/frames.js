83b1771a6d0f7c1a718ee330b00e4f7e6a57a5a6
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-03-22 13:07:15 +0200

    fix: multi protocol example

--- examples/framesjs-starter/app/examples/new-api-multi-protocol/frames/route.tsx
+++ examples/framesjs-starter/app/examples/new-api-multi-protocol/frames/route.tsx
@@ -2,6 +2,7 @@
 import { createFrames, Button } from "frames.js/next";
 import { openframes } from "frames.js/middleware";
 import { defaultMiddleware } from "frames.js/core";
+import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
 
 const totalPages = 5;
 
@@ -10,31 +11,50 @@ export const frames = createFrames({
   initialState: {
     pageIndex: 0,
   },
-  middleware: [...defaultMiddleware, openframes({})],
+  middleware: [
+    ...defaultMiddleware,
+    openframes({
+      clientProtocol: {
+        id: "xmtp",
+        version: "2024-02-09",
+      },
+      handler: {
+        isValidPayload: (body: JSON) => isXmtpFrameActionPayload(body),
+        getFrameMessage: async (body: JSON) => {
+          if (!isXmtpFrameActionPayload(body)) {
+            return undefined;
+          }
+          const result = await getXmtpFrameMessage(body);
+
+          return { ...result };
+        },
+      },
+    }),
+  ],
 });
 
-const handleRequest = frames(async ({ clickedButton, message, state }) => {
-  const imageUrl = `https://picsum.photos/seed/frames.js-${state.pageIndex}/300/200`;
+const handleRequest = frames(async (ctx) => {
+  const imageUrl = `https://picsum.photos/seed/frames.js-${ctx.state.pageIndex}/300/200`;
 
   return {
     image: (
       <div tw="flex flex-col">
         <img width={300} height={200} src={imageUrl} alt="Image" />
         <div tw="flex">
-          This is slide {state?.pageIndex + 1} / {totalPages}
+          This is slide {ctx.state.pageIndex + 1} / {totalPages}
         </div>
       </div>
     ),
     buttons: [
       <Button
         action="post"
-        state={{ pageIndex: (state?.pageIndex - 1) % totalPages }}
+        state={{ pageIndex: (ctx.state.pageIndex - 1) % totalPages }}
       >
         ←
       </Button>,
       <Button
         action="post"
-        state={{ pageIndex: (state?.pageIndex + 1) % totalPages }}
+        state={{ pageIndex: (ctx.state.pageIndex + 1) % totalPages }}
       >
         →
       </Button>,
