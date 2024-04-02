9ef6e806e5dd0f3e715e4d4abe8d80a242f45318
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-04-02 12:14:12 +0200

    build: change eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@framesjs/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
-  },
-  env: {
-    jest: true,
   },
 };
--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -4,15 +4,12 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-  },
+  extends: [
+    require.resolve("@vercel/style-guide/eslint/node"),
+    require.resolve("@vercel/style-guide/eslint/jest-react"),
+    require.resolve("@vercel/style-guide/eslint/react"),
+    require.resolve("@vercel/style-guide/eslint/typescript"),
+  ],
   settings: {
     "import/resolver": {
       typescript: {
@@ -31,4 +28,11 @@ module.exports = {
       files: ["*.js?(x)", "*.ts?(x)"],
     },
   ],
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/require-await": "warn",
+    "@typescript-eslint/no-unnecessary-condition": "warn",
+    "react/jsx-sort-props": "warn",
+    "unicorn/filename-case": "off",
+  },
 };
