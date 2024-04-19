f3d8623dc2d324ce349a53df5275ea7885c6be90
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-04-19 12:01:27 +0200

    feat: allow to override renderer and examples URLs

--- docs/components/Example.tsx
+++ docs/components/Example.tsx
@@ -1,13 +1,41 @@
 type ExampleProps = {
   /**
-   * Full URL to example
+   * Allows to override renderer URL. By default it takes value from process.env.VITE_RENDERER_URL or uses 'http://localhost:3011'
+   *
+   * @defaultValue 'http://localhost:3011'
    */
-  url: string;
+  rendererURL?: string;
+  /**
+   * Allows to override examples domain URL, by default it takes value from process.env.VITE_EXAMPLES_DOMAIN_URL or uses 'http://localhost:3000'
+   *
+   * @defaultValue 'http://localhost:3000'
+   */
+  examplesDomainURL?: string;
+  path: string;
 };
 
-export default function Example({ url }: ExampleProps): JSX.Element {
-  const previewRenderedURL = new URL("http://localhost:3011");
-  previewRenderedURL.searchParams.set('url', url);
+const DEFAULT_RENDERER_URL = process.env.VITE_RENDERER_URL || "http://localhost:3011";
 
-  return <iframe src={previewRenderedURL.toString()} title="Example preview" width={500} height={336} />;
+const DEFAULT_EXAMPLES_DOMAIN_URL =
+  process.env.VITE_EXAMPLES_URL || "http://localhost:3000";
+
+export default function Example({
+  rendererURL = DEFAULT_RENDERER_URL,
+  examplesDomainURL = DEFAULT_EXAMPLES_DOMAIN_URL,
+  path,
+}: ExampleProps): JSX.Element {
+  const previewRenderedURL = new URL(rendererURL);
+  previewRenderedURL.searchParams.set(
+    "url",
+    new URL(path, examplesDomainURL).toString()
+  );
+
+  return (
+    <iframe
+      src={previewRenderedURL.toString()}
+      title="Example preview"
+      width={500}
+      height={336}
+    />
+  );
 }
