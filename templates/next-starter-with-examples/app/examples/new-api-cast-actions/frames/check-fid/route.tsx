4787c9ae99872ac3d994e640e2f656b14102a33a
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-05-08 09:22:16 +0200

    fix(create-frames): move eslint config to cjs, fix eslint errors, use minor versions of nextjs (#371)

--- templates/next-starter-with-examples/app/examples/new-api-cast-actions/frames/check-fid/route.tsx
+++ templates/next-starter-with-examples/app/examples/new-api-cast-actions/frames/check-fid/route.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
 import { frames } from "../frames";
 
@@ -9,7 +10,7 @@ export const POST = frames(async (ctx) => {
   }
 
   return {
-    image: <div tw="flex">The user's FID is {ctx.message?.castId?.fid}</div>,
+    image: <div tw="flex">The user&apos;s FID is {ctx.message?.castId?.fid}</div>,
     buttons: [
       <Button
         action="post"
