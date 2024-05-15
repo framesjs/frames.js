9514e6f345eb135a96d18ec68d1af7f1f115b402
David Furlong <dvfurlong@googlemail.com>
2024-05-15 11:52:27 +0200

    chore: fix docs tailwind config

--- templates/next-starter-with-examples/tailwind.config.ts
+++ templates/next-starter-with-examples/tailwind.config.ts
@@ -9,14 +9,14 @@ const config = {
     "./@/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
     // js files primarily because in dist
-    "./node_modules/frames.js/dist/render/next/*.{ts,tsx,js,css}",
-    "./node_modules/frames.js/dist/render/*.{ts,tsx,js,css}",
     "./node_modules/frames.js/dist/**/*.{ts,tsx,js,css}",
+    "./node_modules/@frames.js/render/dist/*.{ts,tsx,js,css}",
+    "./node_modules/@frames.js/render/dist/**/*.{ts,tsx,js,css}",
 
     // monorepo weirdness
-    "../../node_modules/frames.js/dist/render/next/*.{ts,tsx,js,css}",
-    "../../node_modules/frames.js/dist/render/*.{ts,tsx,js,css}",
     "../../node_modules/frames.js/dist/**/*.{ts,tsx,js,css}",
+    "../../node_modules/@frames.js/render/dist/*.{ts,tsx,js,css}",
+    "../../node_modules/@frames.js/render/dist/**/*.{ts,tsx,js,css}",
   ],
   prefix: "",
   theme: {
