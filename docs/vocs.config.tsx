0d6550c6a2fd12967a05a17fdbd7b11dca387cd1
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-05-23 08:19:49 +0200

    fix(docs): add missing of:accepts meta tag

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -439,6 +439,7 @@ export default defineConfig({
           {/** on production this is rewritten by vercel */}
           <meta property="og:type" content="website" />
           <meta name="fc:frame" content="vNext" />
+          <meta name="of:accepts:farcaster" content="vNext" />
           <meta
             name="fc:frame:post_url"
             content="https://framesjs-homeframe.vercel.app/frames?p=&amp;s=%7B%22page%22%3A1%7D&amp;r=%7B%7D"
