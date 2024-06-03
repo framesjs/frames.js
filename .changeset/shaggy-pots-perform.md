6902eb09cf70812e8cd3928a94d53b1920337a3f
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-06-03 18:50:53 +0200

    feat: custom fonts images worker example

---
+++ .changeset/shaggy-pots-perform.md
@@ -0,0 +1,5 @@
+---
+"template-next-starter-with-examples": patch
+---
+
+feat: custom fonts images worker example
--- templates/next-starter-with-examples/app/examples/images-worker/images/route.tsx
+++ templates/next-starter-with-examples/app/examples/images-worker/images/route.tsx
@@ -1,7 +1,31 @@
 import { createImagesWorker } from "frames.js/middleware/images-worker/next";
+import path from "path";
+import { readFileSync } from "fs";
+
+const interRegular = readFileSync(
+  path.join(path.resolve(process.cwd(), "public"), "Inter-Regular.ttf")
+);
+
+const interBoldFont = readFileSync(
+  path.join(path.resolve(process.cwd(), "public"), "Inter-Bold.ttf")
+);
 
 const imagesRoute = createImagesWorker({
   secret: "SOME_SECRET_VALUE",
+  imageOptions: {
+    fonts: [
+      {
+        name: "Inter",
+        data: interRegular,
+        weight: 400,
+      },
+      {
+        name: "Inter",
+        data: interBoldFont,
+        weight: 700,
+      },
+    ],
+  },
 });
 
 export const GET = imagesRoute();
