4f7b430c9192eb47d810490a8c0022823e21861c
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-06-21 10:54:35 +0200

    chore: change default title

--- packages/frames.js/src/constants.ts
+++ packages/frames.js/src/constants.ts
@@ -1,4 +1,4 @@
 export const FRAMESJS_DEBUG_INFO_IMAGE_KEY =
   "frames.js:debug-info:image" as const;
 
-export const DEFAULT_FRAME_TITLE = "frame";
+export const DEFAULT_FRAME_TITLE = "Frames.js Frame";
