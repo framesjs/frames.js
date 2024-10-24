c5ba758b08d9bbf214ee5bdcc94e089ec78221f5
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-10-24 09:10:41 +0200

    test: skip neynar validation test

--- packages/frames.js/src/middleware/neynar/neynarValidate.test.ts
+++ packages/frames.js/src/middleware/neynar/neynarValidate.test.ts
@@ -64,7 +64,8 @@ describe("neynarValidate middleware", () => {
     expect(next).toHaveBeenCalledWith();
   });
 
-  it("parses frame message from request body and fetches external hub context and adds it to context", async () => {
+  // skipped for now as the default api key doesn't work without browser UA
+  it.skip("parses frame message from request body and fetches external hub context and adds it to context", async () => {
     const context = {
       request: sampleFrameActionRequest.clone(),
     } as unknown as FramesContext;
@@ -95,5 +96,4 @@ describe("neynarValidate middleware", () => {
       })
     );
   });
-
 });
