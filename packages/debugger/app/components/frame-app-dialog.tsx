43801264432efcaae4e7025004fc8e5c3fe3a659
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-11-29 14:28:15 +0100

    feat: add app frame dialog

---
+++ packages/debugger/app/components/frame-app-dialog.tsx
@@ -0,0 +1,126 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useRef, useState } from "react";
+import {
+  useFrameAppInIframe,
+  type FramePrimaryButton,
+} from "@frames.js/render/unstable-use-frame-app";
+import type { LaunchFrameOpenedEvent } from "@frames.js/render/unstable-types";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import type { FarcasterMultiSignerInstance } from "@frames.js/render/identity/farcaster";
+import { Loader2Icon } from "lucide-react";
+import { useWalletClient } from "wagmi";
+import { Button } from "@/components/ui/button";
+
+type FrameAppDialogProps = {
+  farcasterSigner: FarcasterMultiSignerInstance;
+  frameState: Extract<LaunchFrameOpenedEvent, { status: "complete" }>;
+  onClose: () => void;
+};
+
+/**
+ * Frames v2 dialog web view
+ */
+export function FrameAppDialog({
+  farcasterSigner,
+  frameState,
+  onClose,
+}: FrameAppDialogProps) {
+  const walletClient = useWalletClient();
+  const [isReady, setIsReady] = useState(false);
+  const [primaryButton, setPrimaryButton] = useState<FramePrimaryButton | null>(
+    null
+  );
+  const frameApp = useFrameAppInIframe({
+    walletClient,
+    farcasterSigner,
+    frame: frameState.frame,
+    onReady() {
+      setIsReady(true);
+    },
+    onClose,
+    onOpenUrl(url) {
+      window.open(url, "_blank");
+    },
+    onPrimaryButtonSet: setPrimaryButton,
+  });
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const { name, url, splashImageUrl, splashBackgroundColor } =
+    frameState.frame.button.action;
+
+  return (
+    <Dialog
+      open
+      onOpenChange={(isOpen) => {
+        if (!isOpen) {
+          onClose();
+        }
+      }}
+    >
+      <DialogContent className="w-[424px] h-[695px] p-0 gap-0">
+        <DialogHeader className="p-6">
+          <DialogTitle>{frameState.frame.button.action.name}</DialogTitle>
+        </DialogHeader>
+        <div className="relative">
+          {!isReady && (
+            <div
+              className={cn(
+                "bg-white flex items-center justify-center absolute top-0 bottom-0 left-0 right-0"
+              )}
+              style={{ backgroundColor: splashBackgroundColor }}
+            >
+              <div className="w-[200px] h-[200px] relative">
+                <Image
+                  alt={`${name} splash image`}
+                  src={splashImageUrl}
+                  width={200}
+                  height={200}
+                />
+                <div className="absolute bottom-0 right-0">
+                  <Loader2Icon
+                    className="animate-spin text-primary"
+                    size={40}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <iframe
+            className="h-[600px] w-full opacity-100 transition-opacity duration-300"
+            ref={iframeRef}
+            onLoad={frameApp.onLoad}
+            src={url}
+            sandbox="allow-forms allow-scripts allow-same-origin"
+          ></iframe>
+        </div>
+        {primaryButton && !primaryButton.hidden && (
+          <DialogFooter>
+            <Button
+              className="w-full m-1"
+              disabled={primaryButton.disabled || primaryButton.loading}
+              onClick={() => {
+                iframeRef.current?.contentWindow?.dispatchEvent(
+                  new MessageEvent("FarcasterFrameEvent", {
+                    data: {
+                      type: "primaryButtonClicked",
+                    },
+                  })
+                );
+              }}
+              size="lg"
+              type="button"
+            >
+              {primaryButton.text}
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+}
