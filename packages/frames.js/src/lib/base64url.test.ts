86e03b92fbd2812c2699fb7d992ada5057270c7d
Michal Kvasničák <michal.kvasnicak@gmail.com>
2025-01-23 13:14:38 +0100

    feat: support app key signatures (#536)
    * feat: support app key signatures

--- packages/frames.js/src/lib/base64url.test.ts
+++ packages/frames.js/src/lib/base64url.test.ts
@@ -2,20 +2,16 @@ import { base64urlDecode, base64urlEncode } from "./base64url";
 
 describe("base64urlEncode", () => {
   it('works the same as native buffer toString("base64url")', () => {
-    const data = "hello world";
+    const data = Buffer.from("hello world");
 
-    expect(base64urlEncode(data)).toEqual(
-      Buffer.from(data, "utf-8").toString("base64url")
-    );
+    expect(base64urlEncode(data)).toEqual(data.toString("base64url"));
   });
 });
 
 describe("base64urlDecode", () => {
   it('decodes the same as native buffer from("base64url")', () => {
-    const data = base64urlEncode("hello world");
+    const data = base64urlEncode(Buffer.from("hello world"));
 
-    expect(base64urlDecode(data)).toEqual(
-      Buffer.from(data, "base64url").toString("utf-8")
-    );
+    expect(base64urlDecode(data)).toEqual(Buffer.from("hello world"));
   });
 });
